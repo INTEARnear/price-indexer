25370c4cd297a6ac63312ddde448445c786374ed
sliman4 <4sliman4@gmail.com>
2024-06-25 00:10:16 +0300

    Move http server into http_server.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+mod http_server;
 mod pool_data;
 #[cfg(test)]
 mod tests;
@@ -8,14 +9,11 @@ mod utils;
 
 use std::{
     collections::{BTreeMap, HashMap},
-    fs::File,
-    io::BufReader,
     str::FromStr,
     sync::Arc,
 };
 
-use actix_cors::Cors;
-use actix_web::{http::StatusCode, web, App, HttpResponse, HttpResponseBuilder, HttpServer, Route};
+use http_server::launch_http_server;
 use inevents_redis::RedisEventStream;
 use inindexer::near_indexer_primitives::types::AccountId;
 use intear_events::events::{
@@ -29,7 +27,6 @@ use itertools::Itertools;
 use pool_data::{extract_pool_data, PoolData};
 use redis::{aio::ConnectionManager, Client};
 use serde::Deserialize;
-use token::Token;
 use token_metadata::get_token_metadata;
 use tokens::Tokens;
 use tokio::fs;
@@ -84,7 +81,12 @@ async fn main() -> anyhow::Result<()> {
     let last_event_block_height_2 = Arc::new(Mutex::new(None));
 
     let json_serialized_all_tokens = Arc::new(RwLock::new(None));
-    let json_serialized_all_tokens_2 = Arc::clone(&json_serialized_all_tokens);
+
+    tokio::spawn(launch_http_server(
+        Arc::clone(&tokens),
+        Arc::clone(&json_serialized_all_tokens),
+    ));
+
     tokio::spawn(async move {
         loop {
             tokens_clone.write().await.recalculate_prices();
@@ -176,136 +178,6 @@ async fn main() -> anyhow::Result<()> {
         }
     });
 
-    let tokens_2 = Arc::clone(&tokens);
-    tokio::spawn(async move {
-        let tls_config = if let Ok(files) = std::env::var("SSL") {
-            #[allow(clippy::iter_nth_zero)]
-            let mut certs_file =
-                BufReader::new(File::open(files.split(',').nth(0).unwrap()).unwrap());
-            let mut key_file =
-                BufReader::new(File::open(files.split(',').nth(1).unwrap()).unwrap());
-            let tls_certs = rustls_pemfile::certs(&mut certs_file)
-                .collect::<Result<Vec<_>, _>>()
-                .unwrap();
-            let tls_key = rustls_pemfile::pkcs8_private_keys(&mut key_file)
-                .next()
-                .unwrap()
-                .unwrap();
-            Some(
-                rustls::ServerConfig::builder()
-                    .with_no_client_auth()
-                    .with_single_cert(tls_certs, rustls::pki_types::PrivateKeyDer::Pkcs8(tls_key))
-                    .unwrap(),
-            )
-        } else {
-            None
-        };
-
-        let server = HttpServer::new(move || {
-            let tokens = Arc::clone(&tokens_2);
-            let json_serialized_all_tokens = Arc::clone(&json_serialized_all_tokens_2);
-            App::new()
-                .wrap(actix_web::middleware::Logger::new(r#"%a "%r" (forwarded from %{r}a) %s %b Referrer: "%{Referer}i" User-Agent: "%{User-Agent}i" %T"#))
-                .wrap(Cors::default().allow_any_origin().allow_any_method().allow_any_header())
-                .route(
-                    "/list-token-price",
-                    cached_route(Arc::clone(&json_serialized_all_tokens), |json_serialized| {
-                        &json_serialized.ref_compatibility_format
-                    }),
-                )
-                .route(
-                    "/prices",
-                    cached_route(Arc::clone(&json_serialized_all_tokens), |json_serialized| {
-                        &json_serialized.prices_only
-                    }),
-                )
-                .route(
-                    "/super-precise",
-                    cached_route(Arc::clone(&json_serialized_all_tokens), |json_serialized| {
-                        &json_serialized.super_precise
-                    }),
-                )
-                .route("/get-token-price", price_route(Arc::clone(&tokens), |token_id, token| {
-                    HttpResponse::Ok().content_type("text/html; charset=utf8") // why does ref send this as html
-                        .insert_header(("Cache-Control", "public, max-age=1"))
-                        .body(format!(r#"{{"token_contract_id": "{token_id}", "price": "{}"}}"#, token.price_usd.with_scale(12)))
-                }, Some(|token_id| {
-                    HttpResponse::Ok().content_type("text/html; charset=utf8")
-                        .insert_header(("Cache-Control", "public, max-age=1"))
-                        .body(format!(r#"{{"token_contract_id": "{token_id}", "price": "N/A"}}"#))
-                })))
-                .route("/price", price_route(Arc::clone(&tokens), |_, token| {
-                    HttpResponse::Ok()
-                        .insert_header(("Cache-Control", "public, max-age=1"))
-                        .json(token.price_usd.to_string().parse::<f64>().unwrap())
-                }, None))
-                .route("/super-precise-price", price_route(Arc::clone(&tokens), |_, token| {
-                    HttpResponse::Ok()
-                        .insert_header(("Cache-Control", "public, max-age=1"))
-                        .json(token.price_usd.to_string())
-                }, None))
-                .service(
-                    web::scope("/hardcoded")
-                        .route(
-                            "/list-token-price",
-                            cached_route(Arc::clone(&json_serialized_all_tokens), |json_serialized| {
-                                &json_serialized.ref_compatibility_format_with_hardcoded
-                            }),
-                        )
-                        .route(
-                            "/prices",
-                            cached_route(Arc::clone(&json_serialized_all_tokens), |json_serialized| {
-                                &json_serialized.prices_only_with_hardcoded
-                            }),
-                        )
-                        .route(
-                            "/super-precise",
-                            cached_route(Arc::clone(&json_serialized_all_tokens), |json_serialized| {
-                                &json_serialized.super_precise_with_hardcoded
-                            }),
-                        )
-                        .route("/get-token-price", price_route(Arc::clone(&tokens), |token_id, token| {
-                            HttpResponse::Ok().content_type("text/html; charset=utf8") // why does ref send this as html
-                                .insert_header(("Cache-Control", "public, max-age=1"))
-                                .body(format!(r#"{{"token_contract_id": "{token_id}", "price": "{}"}}"#, token.price_usd_hardcoded.with_scale(12)))
-                        }, Some(|token_id| {
-                            HttpResponse::Ok().content_type("text/html; charset=utf8")
-                                .insert_header(("Cache-Control", "public, max-age=1"))
-                                .body(format!(r#"{{"token_contract_id": "{token_id}", "price": "N/A"}}"#))
-                        })))
-                        .route("/price", price_route(Arc::clone(&tokens), |_, token| {
-                            HttpResponse::Ok()
-                                .insert_header(("Cache-Control", "public, max-age=1"))
-                                .json(token.price_usd_hardcoded.to_string().parse::<f64>().unwrap())
-                        }, None))
-                        .route("/super-precise-price", price_route(Arc::clone(&tokens), |_, token| {
-                            HttpResponse::Ok()
-                                .insert_header(("Cache-Control", "public, max-age=1"))
-                                .json(token.price_usd_hardcoded.to_string())
-                        }, None)),
-                    )
-        });
-
-        let server = if let Some(tls_config) = tls_config {
-            server
-                .bind_rustls_0_22(
-                    std::env::var("BIND_ADDRESS").unwrap_or("0.0.0.0:8080".to_string()),
-                    tls_config,
-                )
-                .expect("Failed to bind HTTP server")
-        } else {
-            server
-                .bind(std::env::var("BIND_ADDRESS").unwrap_or("0.0.0.0:8080".to_string()))
-                .expect("Failed to bind HTTP server")
-        };
-
-        server
-            .disable_signals()
-            .run()
-            .await
-            .expect("Failed to start HTTP server");
-    });
-
     RedisEventStream::<TradePoolChangeEventData>::new(
         redis_connection.clone(),
         TradePoolChangeEvent::ID.to_string(),
@@ -337,46 +209,6 @@ async fn main() -> anyhow::Result<()> {
     })
     .await?;
     Ok(())
-}
-
-fn cached_route(
-    json_serialized: Arc<RwLock<Option<JsonSerializedPrices>>>,
-    get_field: fn(&JsonSerializedPrices) -> &String,
-) -> Route {
-    web::get().to(move || {
-        let json_serialized = Arc::clone(&json_serialized);
-        async move {
-            if let Some(json_serialized) = json_serialized.read().await.as_ref() {
-                HttpResponseBuilder::new(StatusCode::OK)
-                    .content_type("application/json")
-                    .insert_header(("Cache-Control", "public, max-age=3"))
-                    .body(get_field(json_serialized).clone())
-            } else {
-                HttpResponse::InternalServerError().finish()
-            }
-        }
-    })
-}
-
-fn price_route(
-    tokens: Arc<RwLock<Tokens>>,
-    respond: fn(AccountId, &Token) -> HttpResponse,
-    respond_404: Option<fn(AccountId) -> HttpResponse>,
-) -> Route {
-    web::get().to(move |query: web::Query<TokenIdWrapper>| {
-        let token_id = query.into_inner().token_id;
-        let tokens = Arc::clone(&tokens);
-        async move {
-            let tokens = tokens.read().await;
-            if let Some(token) = tokens.tokens.get(&token_id) {
-                respond(token_id, token)
-            } else if let Some(respond_404) = respond_404 {
-                respond_404(token_id)
-            } else {
-                HttpResponse::NotFound().finish()
-            }
-        }
-    })
 }
 
 #[derive(Debug, Deserialize)]
