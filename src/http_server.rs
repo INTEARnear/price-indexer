3a64ba082e58006c0d999be912e18589624e597d
sliman4 <4sliman4@gmail.com>
2024-07-28 15:00:19 +0300

    Fix upper-case search term not working

--- src/http_server.rs
+++ src/http_server.rs
@@ -151,7 +151,7 @@ pub async fn launch_http_server(
                         let tokens = Arc::clone(&tokens);
                         async move {
                             let tokens = tokens.read().await;
-                            let results = tokens.search_tokens(&query.query, query.take, query.reputation);
+                            let results = tokens.search_tokens(&query.query.to_lowercase(), query.take, query.reputation);
                             HttpResponse::Ok()
                                 .insert_header(("Cache-Control", "public, max-age=3600"))
                                 .json(results)
